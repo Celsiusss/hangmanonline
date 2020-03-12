63ac1187d8b1dc529ef1bfb06a1fec64ec47e583
Daniel Liland <ende124@users.noreply.github.com>
2020-03-12 02:01:11 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'hmo-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'hangmanonline';
+}
